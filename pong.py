cabd7a94e972e7437de4ca53355731bd595132b2
Kaashvi Jain <159637941+kaashvijain@users.noreply.github.com>
2025-06-26 09:25:42 +0530

    pong.py

--- pong.py
+++ pong.py
@@ -2,16 +2,15 @@ import turtle
 import winsound
 
 wn = turtle.Screen()
-wn.title("Pong by Kaashvi Jain")
+wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Score 
 score_a = 0
 score_b = 0
 
-# Paddle A
+#1st paddle
 paddle_a= turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -20,7 +19,7 @@ paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
 
-# Paddle B
+#2nd paddle
 paddle_b= turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -29,7 +28,7 @@ paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350,0)
 
-# Ball
+#ball
 ball= turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
@@ -39,7 +38,7 @@ ball.goto(0,0)
 ball.dx = 0.05
 ball.dy = 0.05
 
-# Pen
+#pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -48,7 +47,6 @@ pen.hideturtle()
 pen.goto(0,260)
 pen.write("Player A: 0   Player B: 0", align="center", font=("Courier",24,"normal"))
 
-# Functions
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -69,7 +67,7 @@ def paddle_b_down():
     y -= 20
     paddle_b.sety(y)
 
-# Keyboard Input 
+#user-input
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
@@ -77,15 +75,15 @@ wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
 
-# Main game loop
+#main
 while True:
     wn.update()
 
-    # Move the ball
+    #moving the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # Border checking
+    #checking the border
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
@@ -118,7 +116,7 @@ while True:
 
 
 
-    # Paddle and Ball Collisions
+    #collisions bw the paddle and the ball
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
